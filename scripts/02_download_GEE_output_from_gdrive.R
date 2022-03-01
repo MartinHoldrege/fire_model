904dc8f5fec12c38036da59142f718e2a6037d75
Martin Holdrege <martinholdrege@gmail.com>
2022-02-28 18:17:40 -0700

    created script to download gdrive data

---
+++ scripts/02_download_GEE_output_from_gdrive.R
@@ -0,0 +1,45 @@
+# Martin Holdrege
+
+# Script started Feb 28, 2022
+
+# Purpose: download rasters that have been exported to google drive
+# in the 01_compile_data.js script
+
+
+# dependencies ------------------------------------------------------------
+
+library(googledrive)
+
+# get file paths drive --------------------------------------------------------
+
+# files masked to patrick et al fire probability data set
+files_daymet_patrick <- drive_ls(path = 'gee', 
+                                 pattern = '^daymet.*Patrick-etal')
+
+files_rap_patrick <- drive_ls(path = 'gee', 
+                              pattern = 'RAP.*Patrick-etal')
+
+
+# download  ---------------------------------------------------------------
+
+# daymet data
+for (i in 1:nrow(files_daymet_patrick)) {
+  row <- files_daymet_patrick[i, ]
+  drive_download(file = row$id, 
+                 path = file.path("data_processed/daymet", row$name),
+                 overwrite = TRUE)
+}
+
+# RAP data
+for (i in 1:nrow(files_rap_patrick)) {
+  row <- files_rap_patrick[i, ]
+  drive_download(file = row$id, 
+                 path = file.path("data_processed/RAP", row$name),
+                 overwrite = TRUE)
+}
+
+# fire probability
+fire_name <- 'LT_Wildfire_Prob_85to19_v1-0_1000m.tif'
+drive_download(fire_name,
+               path = file.path("data_processed/fire_probability", fire_name),
+               overwrite = TRUE)
