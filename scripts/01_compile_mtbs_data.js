c86147c8091517c62393915ef1f7e2e7bdafd597
mholdrege <mholdrege@gcp.usgs.gov>
2022-03-04 19:18:59 +0000

    WIP--summing fires per pixel

--- scripts/01_compile_mtbs_data.js
+++ scripts/01_compile_mtbs_data.js
@@ -1,3 +1,12 @@
+// User define Variables
+
+// date range
+// this data includes 1984, but to make in comparable to other dfor the time
+// being I'm doing 1985
+var startYear = 1985;
+
+var endYear = 2019;
+
 
 // read in data -------------------------------------------------
 
@@ -27,7 +36,53 @@ var mtbs_boundaries = ee.FeatureCollection("projects/sat-io/open-datasets/MTBS/b
   
 Map.addLayer(mtbs_boundaries, {}, 'mtbs', false);
 
+// testing (remove later)
+var testDate = ee.Date(713775600000);
+print(testDate.millis());
+var test = mtbs_boundaries.filter(ee.Filter.eq('Ig_Date', testDate.millis()))
+  .filter(ee.Filter.eq('Map_ID', 7920));
 
-var test = mtbs_boundaries.select('Event_ID', 'WY4462410655519920814');
-Map.centerObject(test);
-//Map.addLayer(test, {}, "first")
+//Map.centerObject(test);
+Map.addLayer(test, {}, "test", false);
+print(ee.Number(testDate));
+// end testing
+
+// create list of dates
+
+var years = ee.List.sequence(startYear, endYear);
+
+
+// map over years
+
+// List where each element, is a feature collection of the fires that year
+var boundariesByYear = years.map(function(year) {
+  var startTime = ee.Date.fromYMD(year, 1, 1).millis();
+  var endTime = ee.Date.fromYMD(year, 12, 31).millis();
+  var out = mtbs_boundaries
+  //filtering by ignition date
+    .filter(ee.Filter.gte('Ig_Date', startTime))
+    .filter(ee.Filter.lte('Ig_Date', endTime));
+  return out;
+});
+
+Map.addLayer(ee.FeatureCollection(boundariesByYear.get(1)))
+Map.addLayer(ee.FeatureCollection(boundariesByYear.get(2)))
+
+var zero = ee.Image(0);
+
+// CONTINUE here--the next step is to add an attribute of year to each
+// image in the image collection (i.e. startTime), so that layers have time associated with 
+// them--
+var occurrenceByYear = boundariesByYear
+  .map(function(fc) {
+    return zero.paint(fc, 1)//.updateMask(mask); // if fire occured then convert cell to 1
+  });
+
+var occurrenceByYear = ee.ImageCollection(occurrenceByYear);
+
+Map.addLayer(ee.ImageCollection(occurrenceByYear), {palette: ['white', 'black']}, 'single year', false);
+
+// total fires/pixel--this isn't working yet. 
+var firesPerPixel = occurrenceByYear.sum()
+Map.addLayer(firesPerPixel, {palette: ['white', 'black']});
+
