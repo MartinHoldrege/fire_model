a4ef0c7caba0dbb88b945e26baed14e35e268196
mholdrege <mholdrege@gcp.usgs.gov>
2022-03-25 18:42:29 +0000

    WIP

--- scripts/01_compile_fire_data.js
+++ scripts/01_compile_fire_data.js
@@ -23,7 +23,7 @@
  * burned. This could be helpful to get a more complete measure of the 
  * total amount of area burned. 
  * 
- * Note--consider carefully whether to call fall a pixel burned only if the pixel
+ * Note--consider carefully whether to call a pixel burned only if the pixel
  * center intersect the fire polygon, or if any part of it intersects the polygon.
  * This is more important for large (e.g. 1km pixels)
 */
@@ -47,24 +47,10 @@ var run = false;
 var path = 'projects/gee-guest/assets/cheatgrass_fire/';
 // read in annual grass data
 
-// fire probability
-// data from downloaded from: https://doi.org/10.5066/P9ZN7BN8
-// modelled fire probability 1985-2019
-// I'm must using this data for the mask
-var mask = ee.Image(path + 'fire_probability/LT_Wildfire_Prob_85to19_v1-0')
-  .mask();
-
-
-
-// bounding box of the fire data (copied from the metadata of the Pastick fire dataset)
-var region = ee.Geometry({
-  'type': 'Polygon',
-  'coordinates':
-  [[[-121.87704, 47.84742], // NW corner
-  [-104.87570, 36.90701], // SE corner
-  [-110, 40]]],// just another point (inside the bounding box), so creates a triangle, so get get bounds
-  'proj': mask.projection(),
-}).bounds();
+// Mask for the extent of the sagebrush biome
+var m = require("users/mholdrege/cheatgrass_fire:scripts/00_biome_mask.js");
+var mask = m.mask;
+var region = m.region;
 
 // Mtbs data (https://samapriya.github.io/awesome-gee-community-datasets/projects/mtbs/)
 var mtbs_boundaries = ee.FeatureCollection("projects/sat-io/open-datasets/MTBS/burned_area_boundaries");
@@ -316,7 +302,7 @@ var allFiresPerPixelM = allFiresPerPixel.updateMask(mask);
 
 
 var crs = 'EPSG:4326';
-var s =  '_' + startYear + '-' + endYear + '_' + resolution + 'm_pastick-etal-mask_v1';
+var s =  '_' + startYear + '-' + endYear + '_' + resolution + 'm_sagebrush-biome-mask_v1';
 
 if (run) { // set to true of want to export. 
   
