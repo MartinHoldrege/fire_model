a1ea26498e9325ac5e978cc55020783ecd93518a
mholdrege <mholdrege@gcp.usgs.gov>
2022-05-27 20:17:13 +0000

    Updated 02_write_SEI_to_drive.js (added 1 line, deleted 1 line).

--- 02_write_SEI_to_drive.js
+++ 02_write_SEI_to_drive.js
@@ -21,7 +21,7 @@ Map.addLayer(Q5sc3Med, {min:1,max:3, palette: ["black","grey","white"]}, '3 clas
 
 // exported files needs to have USGS albers equal area projection
 // https://spatialreference.org/ref/sr-org/usa-contiguous-albers-equal-area-conic-usgs-version-landfire/
-// The usgs version wasn't recognized, so goin with the regular version
+// The usgs version wasn't recognized, so going with the regular version (EPSG:5070)
 //var wkt = 'PROJCS["USA_Contiguous_Albers_Equal_Area_Conic_USGS_version",GEOGCS["GCS_North_American_1983",DATUM["D_North_American_1983",SPHEROID["GRS_1980",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Albers"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",-96.0],PARAMETER["Standard_Parallel_1",29.5],PARAMETER["Standard_Parallel_2",45.5],PARAMETER["Latitude_Of_Origin",23.0],UNIT["Meter",1]]';
 
 //var proj_usgs = ee.Projection(wkt);
