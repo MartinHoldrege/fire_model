d092da28c851367b7df3ac0cd4eb862fa49de73b
Martin Holdrege <martinholdrege@gmail.com>
2023-06-22 17:29:44 -0600

    created csv for data publication

--- scripts/data_publication_products.R
+++ scripts/data_publication_products.R
@@ -2,13 +2,16 @@
 
 # Script started 3/9/2023
 
-# Purpose: create a cell number 'index' raster, where each cell
+# Purpose: create files for science base data publication. 
+# first create a cell number 'index' raster, where each cell
 # is the cell number which is used as an identifier in various dataframes
+# and also a table of data (predictor and response variables used in models)
 
 
 # dependencies ------------------------------------------------------------
 
-library(terra)
+# creates the dataframe (containing data used to fit models) df_byNFire2_hmod
+source("scripts/04_create_biome-mask_dataframe_byNFire.R")
 
 
 # read in data ------------------------------------------------------------
@@ -16,6 +19,8 @@ library(terra)
 # use one of the climate rasters as a template
 r <- rast("data_processed/daymet/daymet_climYearlyAvg_1986-2019_1000m_sagebrush-biome-mask_v1.tif")
 
+# the final model, described in the manuscript submitted to fire ecology
+mod <- readRDS("models/glm_binomial_models_byNFire_v2_bin20_cwf_A-P_A2-T2_A-Pr.RDS")[[1]]
 
 # create cellNum raster ---------------------------------------------------
 
@@ -25,5 +30,35 @@ cellNums <- r
 cellNums[!is.na(r)] <- terra::cells(r)
 plot(cellNums)
 
-writeRaster(cellNums, "data_processed/cell_nums.tif",
+
+
+
+# create dataframe for output ----------------------------------------------
+
+df1 <- df_byNFire2_hmod
+df1$predicted_prob <- predict(mod, newdata = df1, type = "response")
+
+df2 <- df1 %>% 
+  select(-herbAGB, -occur_cwf, -weight, -cwf_prop) %>% 
+  rename(nfire = nfire_cwf) %>% 
+  select(cell_num, cell_size, nfire, predicted_prob, everything())
+
+
+# *summary stats -----------------------------------------------------------
+# column summary stats for terry
+summaries <- df2 %>% 
+  pivot_longer(cols = everything(),
+               names_to = "column_name") %>% 
+  group_by(column_name) %>% 
+  summarize(min = min(value, na.rm = TRUE),
+            max = max(value, na.rm = TRUE))
+
+# output data -------------------------------------------------------------
+
+writeRaster(cellNums, "data_processed/data_publication/cell_nums.tif",
             overwrite = TRUE)
+
+write_csv(df2, "data_processed/data_publication/fire_climate_vegetation.csv")
+
+
+write_csv(summaries, "data_processed/data_publication/data_pub_col_summaries.csv")
