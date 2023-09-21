7718dc9bfee846a75da3d4a0a52bbb41e1b8e739
Martin Holdrege <martinholdrege@gmail.com>
2023-09-21 14:17:18 -0600

    added comparisons with Short 2023 and pastick 2021

--- scripts/06_compare-to-other-datasets.R
+++ scripts/06_compare-to-other-datasets.R
@@ -6,7 +6,7 @@
 # to fire probability (or other relative metrics of fire probability) from
 # other studies particularly Short et al 2020 (FSim model, US wide dataset) and
 # Smith et al 2022 (Random forest model, great basin dataset)
-
+# Short et al 2023 (FSim sagebrush dataset)
 
 # dependencies ------------------------------------------------------------
 
@@ -20,7 +20,7 @@ source("src/general_functions.R")
 # reference raster giving cell numbers that correspond to unique identifiers
 # in other data frames
 
-cell_nums <- rast("data_processed/cell_nums.tif")
+cell_nums <- rast("data_processed/data_publication/cell_nums.tif")
 
 
 # * model object ----------------------------------------------------------
@@ -45,6 +45,14 @@ fsim1 <- rast('data_raw/fsim_fire_prob_RDS-2016-0034-2/Data/I_FSim_CONUS_LF2014_
 # * smith2020 -------------------------------------------------------------
 
 smith1 <- rast("data_processed/fire_probability/smith2022_mean_annual_fire_prob_1988-2019.tif")
+
+
+# * short2023 -------------------------------------------------------------
+# Similar approach to Fsim dataset above (I think) but specifically run for the sagebrush region
+# dataset: https://doi.org/10.2737/RDS-2023-0050
+
+short1 <- rast('data_raw/SBRA_BP/SBRA_BP.tif')
+
 
 # line up rasters ---------------------------------------------------------
 
@@ -71,7 +79,6 @@ df_fsim <- get_values(lyr = 'fsim', r = fsim3) %>%
 # * smith -----------------------------------------------------------------
 
 smith2 <- project(smith1, cell_nums, method = 'bilinear')
-plot(smith2)
 
 compareGeom(smith2, cell_nums,  crs = TRUE, ext = TRUE,
             rowcol = TRUE)
@@ -79,6 +86,18 @@ compareGeom(smith2, cell_nums,  crs = TRUE, ext = TRUE,
 df_smith <- get_values(lyr = 1, r = smith2) %>% 
   rename('smith' = 'value') %>% 
   select(-lyr)
+
+
+# * short -----------------------------------------------------------------
+short2 <- project(short1, cell_nums, method = 'bilinear')
+
+compareGeom(short2, cell_nums,  crs = TRUE, ext = TRUE,
+            rowcol = TRUE)
+
+df_short <- get_values(lyr = 1, r = short2) %>% 
+  rename('short' = 'value') %>% 
+  select(-lyr)
+
 
 # predicted fire probability ----------------------------------------------
 
@@ -88,16 +107,18 @@ pred <- predict_cell_avg(mod, newdata = data1, type = 'response')
 
 # compare to others -------------------------------------------------------
 
-
 data2 <- pred %>% 
   left_join(df_fsim, by = "cell_num") %>% 
-  left_join(df_smith, by = "cell_num")
+  left_join(df_smith, by = "cell_num") %>% 
+  left_join(df_short, by = 'cell_num')
 
 cor(data2$pred, data2$fsim, use = "complete.obs")
 cor(data2$pred, data2$smith, use = "complete.obs")
 # 0.74
+cor(data2$pred, data2$short, use = "complete.obs")
+# 0.64
 cor(data2$fsim, data2$smith, use = "complete.obs")
-
+cor(data2$fsim, data2$short, use = "complete.obs")
 
 data3 <- data2 %>% 
   drop_na()
@@ -135,3 +156,14 @@ data2 %>%
        y = "Relative wildfire probability (Smith et al. 2022)")
 dev.off()
 
+png("figures/comparison_with_short2023_v1.png",
+    width = 4, height = 4, units = "in", res = 600)
+data2 %>% 
+  sample_n(1*10^5) %>% 
+  ggplot(aes(pred*100, short*100)) +
+  geom_point(alpha = 0.05, size = 0.5) +
+  labs(x = xlab,
+       y = "Burn probability (Short et al. 2023) (converted to %)") +
+  geom_smooth(method = 'lm', se = FALSE) +
+  geom_abline(slope = 1)
+dev.off()
