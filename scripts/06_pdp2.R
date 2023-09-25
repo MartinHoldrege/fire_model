a3f2cb39bdf908030597cdc00363aa3aa5a38644
Martin Holdrege <martinholdrege@gmail.com>
2023-09-25 14:48:29 -0600

    finished draft pdp plots showing high and low curves for interacting vars

--- scripts/06_pdp2.R
+++ scripts/06_pdp2.R
@@ -22,41 +22,43 @@ theme_set(theme_classic())
 # string vector, part of the name of the model, usually identifying
 # the model interactions
 
-s "_A-P_A2-T2_A-Pr"#, # model 4b (final model)
+s <- "_A-P_A2-T2_A-Pr"#, # model 4b (final model)
 
 limit_axes <- FALSE
 
-  # read in model objects ---------------------------------------------------
-  
-  # main model
-  mods <- readRDS(paste0("models/glm_binomial_models_byNFire_v2_bin20_cwf",
-                         s, ".RDS"))
-  mod <- mods$paint_cwf
-  
+# read in model objects ---------------------------------------------------
+
+# main model
+mods <- readRDS(paste0("models/glm_binomial_models_byNFire_v2_bin20_cwf",
+                       s, ".RDS"))
+mod <- mods$paint_cwf
 
 
-  # create pdp --------------------------------------------------------------
-  
-  # lookup table
-  lookup_var <- var2lab(x = NULL, units_md = TRUE)
-  mod_vars <- names(lookup_var)
-  names(mod_vars_h) <- mod_vars
-  
 
+# create pdp --------------------------------------------------------------
+
+# lookup table
+lookup_var <- var2lab(x = NULL, units_md = TRUE)
+mod_vars <- names(lookup_var)
+names(mod_vars) <- mod_vars
+  
 # * prepare 'training' datasets -------------------------------------------
 # datasets with a given variable fixed at a certain percentile. 
+
 train1 <- mod$data 
-train1 <- slice_sample(train1, n = 1000) # for testing
 
-quantiles <- map(train1[mod_vars], \(x) quantile(x, probs = c(0.2, 0.8)))
+# train1 <- slice_sample(train1, n = 1000) # for testing
 
+probs <- c(0.0001, 0.9999)
+quantiles <- map(train1[mod_vars], \(x) quantile(x, probs = probs))
+quantiles
 vars <- quantiles[c('MAT', 'MAP', 'pfgAGB', 'afgAGB')]
 train_l1 <- map2(quantiles, names(quantiles), function(quant, var) {
   out <- list()
   df <- train1
-  df[[var]] <- quant['20%']
+  df[[var]] <- quant[1]
   out$low <- df
-  df[[var]] <- quant['80%']
+  df[[var]] <- quant[2]
   out$high <- df
   out
 })
@@ -116,9 +118,7 @@ df_pdp2 <- df_pdp1 %>%
   mutate(xlab = lookup_var[variable],
          xlab = factor(xlab, levels = unique(xlab)))
 
-ggplot(df_pdp2, aes(x_value, yhat, color = inter_var)) +
-  geom_line() +
-  facet_wrap(~xlab, scales = 'free_x')
+
   
 # for rug plot 
 deciles <- train1 %>% 
@@ -139,171 +139,55 @@ deciles <- train1 %>%
          decile != min(decile))
   
 
-  # reduce x axis limits? 
-  df_pdp3 <- if(limit_axes) {
-    df_pdp2 %>% 
-      left_join(xlims, by = c("variable" = "var")) %>% 
-      filter(x_value <= max & x_value >= min) %>% 
-      select(-max, -min)
-  } else {
-    df_pdp2
-  }
+
+letter_df <- tibble(
+  letter = fig_letters[1:length(mod_vars)],
+  xlab = factor(lookup_var),
+  x_value = -Inf,
+  yhat = Inf
+)
+
+
+base_pdp <- function(limit_x = FALSE) {
+  out <- list(
+    facet_wrap(~xlab, scales = 'free', strip.position = "bottom"),
+    theme(strip.text = element_markdown(),
+          strip.placement = "outside",
+          axis.title.x = element_blank(),
+          strip.background = element_blank()),
+    labs(y = "Annual fire probability (%)"),
+    sec_axis_fri(), # adding second axis (fire return interval)
+    "limit_x" = ggh4x::facetted_pos_scales(
+      x = list(xlab == lookup_var['MAP'] ~ scale_x_continuous(limits = c(0, 1000)),
+               xlab == lookup_var['MAT'] ~ scale_x_continuous(limits = c(0, 20)))
+    ))
   
-  
-  letter_df_h <- tibble(
-    letter = fig_letters[1:length(mod_vars)],
-    xlab = factor(lookup_var),
-    x_value = -Inf,
-    yhat = Inf
-  )
-  
-  letter_df <- letter_df_h %>% 
-    filter(xlab!= lookup_var_h['hmod'])
-  
-  base_pdp <- function(limit_x = FALSE) {
-    out <- list(
-      facet_wrap(~xlab, scales = 'free', strip.position = "bottom"),
-      theme(strip.text = element_markdown(),
-            strip.placement = "outside",
-            axis.title.x = element_blank(),
-            strip.background = element_blank()),
-      labs(y = "Annual fire probability (%)"),
-      sec_axis_fri(), # adding second axis (fire return interval)
-      expand_limits(y = c(0, 3)),
-      "limit_x" = ggh4x::facetted_pos_scales(
-        x = list(xlab == lookup_var['MAP'] ~ scale_x_continuous(limits = c(0, 1000)),
-                 xlab == lookup_var['MAT'] ~ scale_x_continuous(limits = c(0, 20)))
-      ))
-    
-    if(!limit_x) {
-      out[["limit_x"]] <- NULL
-    }
-    out
-  }
-  
-  # for naming
-  v <- if(limit_axes) {
-    "v2"
-  } else {
-    "v3"
-  }
-  
-  
-  png(paste0("figures/pdp/pdp_pub-qual_", v, s, ".png"), 
-      units = "in", res = 600,  width = 7, height = 3.5)
-  g <- ggplot(df_pdp3, aes(x_value, yhat*100)) +
-    geom_line() +
-    geom_rug(data = deciles, aes(x = decile, y = NULL), sides = 'b') +
-    geom_text(data = letter_df, aes(label = letter),
-              hjust = -0.8,
-              vjust = 1) +
-    # if limit_axes is true, then dataset was already filtered,
-    # so additional limits should not be set
-    base_pdp(limit_x = !limit_axes) 
-  
-  print(g)
-  dev.off()
-  
-  
-  # create pdp hmod------------------------------------------------------------
-  
-  dfs_pdp_h <- map(mod_vars_h, function(var) {
-    pdp::partial(mod_h, pred.var = var, plot = FALSE,
-                 prob = TRUE, train = mod_h$data)
-  }) 
-  
-  dfs_pdp_h$MAT$MAT <- dfs_pdp_h$MAT$MAT - 273.15 # k to c
-  df_pdp1_h <- map(dfs_pdp_h, function(df) {
-    var_name <- names(df)[1]
-    
-    out <- as_tibble(df)
-    names(out) <- c("x_value", "yhat")
-    out$variable <- var_name
-    out
-  }) %>% 
-    bind_rows() 
-  
-  
-  df_pdp2_h <- df_pdp1_h %>% 
-    mutate(xlab = lookup_var_h[variable],
-           xlab = factor(xlab, levels = unique(xlab)))
-  
-  # for rug plot 
-  deciles_h <- mod_h$data %>% 
-    select(all_of(names(lookup_var_h))) %>% 
-    map(quantile, probs = seq(0, 1, 0.1)) %>% 
-    bind_cols() %>% 
-    mutate(MAT = MAT - 273.15 # k to c
-    ) %>% 
-    pivot_longer(cols = everything(), 
-                 names_to = "variable",
-                 values_to = "decile") %>% 
-    mutate(xlab = lookup_var_h[variable],
-           xlab = factor(xlab, levels = unique(xlab)))
-  
-  # checking that the hmod and regular mod 
-  # have the same interactions (i.e. are comparable)
-  x <- mods_h$pred_vars_inter
-  y <- mods$pred_vars_inter
-  
-  
-  # note that some of the older model objects don't include the pred_vars_inter
-  # list element
-  # this if statement is included to help insure that figure is only
-  # created for comparable models (ie only difference is presence of hmod term in)
-  if(all(x[x!='hmod'] == y)) {
-    g <- ggplot(df_pdp2_h, aes(x_value, yhat*100)) +
-      geom_line(aes(color = 'Model with human modification',
-                    linetype = 'Model with human modification')) +
-      geom_rug(data = deciles_h, aes(x = decile, y = NULL), sides = 'b') +
-      geom_text(data = letter_df_h, aes(label = letter),
-                hjust = -0.8,
-                vjust = 1) +
-      base_pdp(limit_x = TRUE) +
-      geom_line(data = df_pdp2, aes(color = "Original model",
-                                    linetype = "Original model")) +
-      theme(legend.title = element_blank(),
-            legend.position = "top") +
-      scale_color_manual(values = c("black", "blue"), name = 'name') +
-      scale_linetype_manual(values = c(1, 2), name = 'name')
-    
-    png("figures/pdp/pdp_pub-qual_hmod_v3.png", units = "in", res = 600,
-        width = 6, height = 3.5)
-    print(g)
-    
-    dev.off()
-  }
-  
-  
-  
-  # find maxima -------------------------------------------------------------
-  # values of the predictor variable for which the probability (from the pdp)
-  # is hightest
-  
-  df_pdp2 %>% 
-    group_by(variable) %>% 
-    filter(yhat == max(yhat))
-  
-  
-  # output rounded coefficients ----------------------------------------------
-  
-  # useful for copy and pasting into manuscript
-  coef_df <-  map_dfr(list(HMod = mod_h, main = mod), function(x) {
-    out <- summary(x) %>% 
-      .$coefficients %>% 
-      as_tibble() %>% 
-      mutate(variable = names(x$coefficients),
-             Estimate = map_chr(Estimate, format,scientific = F, digits = 4),
-             `Std. Error` = map_chr(`Std. Error`, format,scientific = F, digits = 3),
-             `z value` =  map_chr(`z value`, format,scientific = F, digits = 3))
-    out
-  },
-  .id = "model")
-  
-  if(all(x[x!='hmod'] == y)) {
-    write_csv(coef_df, "models/models_coefs_glm_byNFire_bin20.csv")
-  }
-  
-  
+  out
 }
 
+ggplot(df_pdp2, aes(x_value, yhat, color = inter_var)) +
+  geom_line() +
+  facet_wrap(~xlab, scales = 'free')
+png(paste0("figures/pdp/pdp_high-low_v2", s, ".png"),
+    units = "in", res = 600,  width = 8, height = 3.5)
+ggplot(df_pdp2, aes(x_value, yhat*100)) +
+  geom_line(aes(color = inter_var)) +
+  geom_rug(data = deciles, aes(x = decile, y = NULL), sides = 'b') +
+  geom_text(data = letter_df, aes(label = letter),
+            hjust = -0.8,
+            vjust = 1) +
+  # if limit_axes is true, then dataset was already filtered,
+  # so additional limits should not be set
+  base_pdp() +
+  scale_color_manual(values = c('#006837', '#66bd63',
+                       '#2166ac', '#92c5de',
+                       '#b2182b', '#f4a582',
+                       'black',
+                       '#66c2a5', '#abdda4')) +
+  labs(caption = paste('percentiles used:', paste0(probs*100, "%", collapse = ', ')))
+
+
+dev.off()
+  
+  
+ 
