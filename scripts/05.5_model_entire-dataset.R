c6fdd1559b55bb10f9dad9ab530a40674ec9c6a4
Martin Holdrege <martinholdrege@gmail.com>
2023-10-18 10:15:24 -0600

    fit mod with sqrt(A):P interaction

--- scripts/05.5_model_entire-dataset.R
+++ scripts/05.5_model_entire-dataset.R
@@ -20,10 +20,11 @@ library(dplyr)
 
 # read in data ------------------------------------------------------------
 
-df_ann <- read_csv("data_processed/fire-clim-veg_3yrAvg_v1.csv",
+# burn occurrence determined from fraction burned
+df_ann <- read_csv("data_processed/fire-clim-veg_3yrAvg_v2.csv",
                    show_col_types = FALSE) 
 
-# confirming the model objects all have the same final formula
+# confirming the model objects all have the same final formula (update as needed)
 if(FALSE) {
   paths <- list.files("models", pattern = 'glm_binomial_models_v1_ann.*',
                       full.names = TRUE)
@@ -41,7 +42,8 @@ if(FALSE) {
 
 # model fitting -----------------------------------------------------------
 set.seed(1234)
-form <- "cwf_prop ~ sqrt(afgAGB) + stats::poly(pfgAGB,2,raw=TRUE) + stats::poly(MAT,2,raw=TRUE) + stats::poly(MAP,2,raw=TRUE) + stats::poly(prcpPropSum,2, raw = TRUE) + (afgAGB:MAP)"
+# hard coding this model (for now)
+form <- "cwf_prop ~ sqrt(afgAGB) + stats::poly(pfgAGB,2,raw=TRUE) + stats::poly(MAT,2,raw=TRUE) + stats::poly(MAP,2,raw=TRUE) + stats::poly(prcpPropSum,2, raw = TRUE) + (sqrt(afgAGB):MAP)"
 
 
 m1 <- glm(as.formula(form), family = binomial(link = 'logit'),
@@ -56,5 +58,5 @@ mod2save <- butcher::butcher(m1) # removes some model components so the saved ob
 
 mod2save$formula <- form
 
-saveRDS(mod2save, "models/glm_binomial_models_v1_ann_A-P_entire.RDS")
+saveRDS(mod2save, "models/glm_binomial_models_v2_ann_sA-P_entire.RDS")
 
